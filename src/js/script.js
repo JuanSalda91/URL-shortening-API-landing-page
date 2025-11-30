b438e1ca5690d21aacaae14547c30324bc7f0e98
JuanSalda91 <juansalda9131@gmail.com>
2025-11-30 16:13:13 -0500

    HTML elements selected, div(container) element created to add generated URLs, API function created

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,51 @@
 const inputField = document.querySelector('input[type="text"]');
 const shortenBtn = document.querySelector('.input-btn');
 const form = document.querySelector('.input-container');
+
+// ----- New links container ----- //
+
+const resultsContainer = document.createElement('div');
+resultsContainer.id = 'result-area';
+resultsContainer.style.marginTop = '2rem';
+resultsContainer.style.width = '100%';
+resultsContainer.style.maxWidth = '100px';
+resultsContainer.style.display = 'flex';
+resultsContainer.style.dlexDirection = 'column';
+resultsContainer.style.gap = '1rem'
+resultsContainer.style.alignItems = 'center';
+
+// --- inserting link's results into the container --- //
+
+document.querySelector('.link-input').appendChild(resultsContainer);
+
+// ----- API ----- //
+
+async function shortenUrl(longUrl) {
+    const accessToken = '432422d832823c840f07c6636b5356989360e479';
+    const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json';
+            },
+            body: JSON.stringify({
+                long_Url: longUrl,
+                domain: "bit.ly"
+            })
+        });
+        
+        if(!noresponde.ok) {
+            throw new Error('Failed to shorten URL');
+        }
+
+        const data = await response.json();
+        return data.link; // -- shortened URL -- //
+    } catch (error) {
+        console.error('ERROR:', error);
+        alert('Something went worng! PLease check the URL or your connection.')
+        return null;
+    }
+}
