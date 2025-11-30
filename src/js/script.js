e86b4a1401f18397c721b819393b0d99f9f0acc8
JuanSalda91 <juansalda9131@gmail.com>
2025-11-30 15:45:31 -0500

    js file init

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+// Access token: 432422d832823c840f07c6636b5356989360e479 //
+
+// ----- Getting HTML ----- //
+const inputField = document.querySelector('input[type="text"]');
+const shortenBtn = document.querySelector('.input-btn');
+const form = document.querySelector('.input-container');
